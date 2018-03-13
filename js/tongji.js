ff83d1fdfe6099e4c1759e48af4a4b9eddc54fbb
taozimom <taozimom@qq.com>
2018-03-13 17:15:31 +0800

    tongji

---
+++ js/tongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?e9df24fdcce1cc7b468d6f778bd717de";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
